e6a47ed4479adeba573391ac7428e1f733c5914c
IslamAhmed0 <ea43533@gmail.com>
2021-04-22 04:15:09 +0200

    labs

---
+++ lab_two_1/js/books.js
@@ -0,0 +1,24 @@
+books= [
+    {
+
+        ISBN: 0,
+        bookimg: './img/js.jpg',
+        bookName: 'Angular',
+        category: 'web development',
+        price: 50,
+        booknumberogbage:2,
+        author: "islam",
+    },
+    {
+
+        ISBN: 7,
+        bookimg: './img/js.jpg',
+        bookName: 'Angular',
+        category: 'web development',
+        price: 50,
+        booknumberogbage: 10,
+        author: "islam",
+    },
+
+
+]
